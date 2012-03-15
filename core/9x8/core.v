5079e84f716af58dafcec90da5eb1cacb4760bdb
Rodney Sinclair <rsinclair@sinclairrf.com>
2012-03-15 14:00:46 -0700

    Work on numerical values for the instruction opcodes

--- core/9x8/core.v
+++ core/9x8/core.v
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ *
+ * The processor core is implemented such that at most two layers of 6-input
+ * LUTs are involved in the computations between successive clock cycles.  For
+ * example, if an addition operation is performed then
+ *   1. Do the unlatched addition and identify how the registers will be
+ *      affected.  Here, the input to the T latch will be the adder output, the
+ *      input to N will be the top of the data stack, the data stack pointer
+ *      will be decremented, the return stack pointer will not be afffected, and
+ *      the program counter will proceed normally.
+ *   2. Do the latched operations on the registers.  Here, T will be latched as
+ *      the adder output, N will be latched as the old top of the data stack,
+ *      etc.
+ *
+ ******************************************************************************/
+
 reg       [7:0] T                       = 8'h00;
 
 wire            conditional_true        = |T;
@@ -23,27 +39,58 @@ always @ (posedge i_clk)
     T <= T;
   end
 
-wire       math_adder_opcode = opcode_curr[0];
-wire [7:0] math_adder_out;
-always @ (T,N,math_adder_opcode)
-  if (math_adder_opcode)
-    math_adder_out <= N + T;
-  else
-    math_adder_out <= N - T;
+/*******************************************************************************
+ *
+ * First stage operations.
+ *
+ ******************************************************************************/
 
-wire [2:0] math_opcode  = opcode_curr[0+:3];
-wire [7:0] math_out;
-always @ (T,N,math_out)
-  case (math_opcode)
-     3'b000 : math_out <= T;                    // nop
-     3'b001 : math_out <= {(8){!(|T)}};         // 0=
-     3'b010 : math_out <= { T[0+:7], 1'b0 };    // 2*
-     3'b011 : math_out <= { 1'b0, T[1+:7] };    // 2/
-     3'b100 : math_out <= T & N;                // and
-     3'b101 : math_out <= T | N;                // or
-     3'b110 : math_out <= T ^ N;                // xor
-     3'b111 : math_out <= N;                    // part of "over"
-    default : math_out <= T;
+// adder/subtracter
+wire [7:0] math_dual_adder;
+always @ (T,N,opcode_curr)
+  case (opcode_curr[0])
+       1'b0 : math_dual_adder <= N + T; // add
+       1'b1 : math_dual_adder <= N - T; // sub
+    default : math_dual_adder <= T;
+  endcase
+
+// logic operations
+// 4-input LUT formulation -- 2-bit opcode, 1 bit each of T and N
+wire [7:0] math_dual_logic;
+always @ (T,N,opcode_curr)
+  case (opcode_curr[0+:2])
+      2'b00 : math_dual_logic <= T & N; // and
+      2'b01 : math_dual_logic <= T | N; // or
+      2'b10 : math_dual_logic <= T ^ N; // xor
+      2'b11 : math_dual_logic <= T;     // nip
+    default : math_dual_logic <= T;
+  endcase
+
+// shifter operations (including "nop" as no shift)
+// 6-input LUT formulation -- 3-bit opcode, 3 bits of T centered at current bit
+wire [7:0] math_rotate;
+always @ (T,opcode_curr)
+  case (opcode_curr[0+:3])
+     3'b000 : math_rotate <= T;                 // nop
+     3'b001 : math_rotate <= { T[0+:7], 1'b0 }; // <<0
+     3'b010 : math_rotate <= { T[0+:7], 1'b1 }; // <<1
+     3'b011 : math_rotate <= { T[0+:7], T[0] }; // <<lsb
+     3'b100 : math_rotate <= { 1'b0, T[1+:7] }; // 0>>
+     3'b101 : math_rotate <= { 1'b1, T[1+:7] }; // 1>>
+     3'b110 : math_rotate <= { T[7], T[1+:7] }; // msb>>
+     3'b111 : math_rotate <= { T[0], T[1+:7] }; // lsb>>
+    default : math_rotate <= T;
+  endcase
+
+// T pre-multiplexer for pushing repeated values onto the data stack
+wire [7:0] s_T_pre;
+always @ (*)
+  case (opcode_curr[0+:2])
+      2'b00 : s_T_pre <= s_T;                   // dup
+      2'b01 : s_T_pre <= s_R[0+:8];             // r@
+      2'b10 : s_T_pre <= s_N;                   // over
+      2'b11 : s_T_pre <=
+    default : s_T_pre <= s_T;
   endcase
 
 /*******************************************************************************
@@ -71,7 +118,7 @@ localparam C_BUS_T_PRE_INPORT   = 3'b101;
 localparam C_BUS_T_NOP          = 2'b00;
 localparam C_BUS_T_PRE          = 2'b01;
 localparam C_BUS_T_MATH_DUAL    = 2'b10;
-localparam C_BUS_T_MATH_SINGLE  = 2'b11;
+localparam C_BUS_T_MATH_ROTATE  = 2'b11;
 
 localparam C_BUS_N_NOP          = 2'b00;
 localparam C_BUS_N_T            = 2'b01;
@@ -106,64 +153,69 @@ always @ (opcode_curr,T) begin
     s_stack     <= C_STACK_DEC;
     s_interrupt_holdoff <= 1'b1;
   end else case (opcode_curr[3+:4])
-      4'b0000:  // nop
-                ;
-      4'bXXXX:  // drop, outport, callc, >r
-                s_bus_t_pre     <= C_BUS_T_PRE_N;
-                s_bus_t         <= C_BUS_T_PRE;
+      4'b0000:  // nop, math_rotate
+                s_bus_t         <= C_BUS_T_MATH_ROTATE;
                 s_bus_n         <= C_BUS_N_STACK;
-                s_stack         <= C_STACK_DEC;
-                s_outport_next  <= opcode_curr[0];
-                if (opcode_curr[1] && (|T)) begin
-                  s_bus_pc      <= C_BUS_PC_JUMP;
-                  s_bus_r       <= C_BUS_R_PC;
-                end
-                if (opcode_curr[2])
-                  s_bus_r       <= C_BUS_R_T;
-      4'bXXXX:  // dup
-                s_bus_t_pre     <= C_BUS_T_PRE_T;
+                s_stack         <= C_STACK_NOP;
+      4'b0001:  // dup, r@, over
                 s_bus_t         <= C_BUS_T_PRE;
                 s_bus_n         <= C_BUS_N_T;
                 s_stack         <= C_STACK_INC;
-      4'bXXXX:  // swap
-                s_bus_t_pre     <= C_BUS_T_PRE_N;
+      4'b0010:  // swap
                 s_bus_t         <= C_BUS_T_PRE;
                 s_bus_n         <= C_BUS_N_T;
-      4'bXXXX:  // @ (fetch)
-                s_bus_t_pre     <= C_BUS_T_PRE_MEMORY;
-                s_bus_t         <= C_BUS_T_PRE;
-      4'bXXXX:  // ! (store)
-                ?
-      4'bXXXX:  // inport
-                s_bus_t_pre     <= C_BUS_T_PRE_INPORT;
-                s_bus_t         <= C_BUS_T_PRE;
-      4'bXXXX:  // dual-operand math:  add/and/or/sub/xor
-                s_bus_t         <= C_BUS_T_MATH_DUAL;
+      4'b0011:  // dual-operand math:  add/sub
+                s_bus_t         <= C_BUS_T_MATH_ADDER;
                 s_bus_n         <= C_BUS_N_STACK;
                 s_stack         <= C_STACK_DEC;
-      4'bXXXX:  // single-operand math: 0=, 2*, 2/
-                s_bus_t         <= C_BUS_T_MATH_SINGLE;
-      4'bXXXX:  // return
+      4'b0100:  // dual-operand math:  and/or/xor/nip
+                s_bus_t         <= C_BUS_T_MATH_LOGIC;
+                s_bus_n         <= C_BUS_N_STACK;
+                s_stack         <= C_STACK_DEC;
+      4'b0101:  // return
                 s_bus_pc        <= C_BUS_PC_RETURN;
-      4'bXXXX:  // r@ (push a copy of the top of the return stack onto the data stack)
-                // r> (pop the return stack and push it onto the data stack)
-                s_bus_t_pre     <= C_BUS_T_PRE_RETURN;
+                s_return        <= C_RETURN_DEC;
+      4'b0110:  // inport
+                s_bus_t         <= C_BUS_T_INPORT;
+      4'b0111:  // outport
+                s_bus_t         <= C_BUS_T_PRE;
+                s_bus_n         <= C_BUS_N_STACK;
+                s_stack         <= C_STACK_DEC;
+      4'b1000:  // call
+                s_bus_pc        <= C_BUS_PC_JUMP;
+                s_return        <= C_RETURN_INC;
+                s_bus_r         <= C_BUS_R_PC;
+      4'b1001:  // callc
+                s_bus_pc        <= C_BUS_PC_JUMP;
+                s_return        <= C_RETURN_INC;
+                s_bus_r         <= C_BUS_R_PC;
+                s_bus_t         <= C_BUS_T_PRE;
+                s_bus_n         <= C_BUS_N_STACK;
+                s_stack         <= C_STACK_DEC;
+      4'b1010:  // drop
+                s_bus_t         <= C_BUS_T_PRE;
+                s_bus_n         <= C_BUS_N_STACK;
+                s_stack         <= C_STACK_DEC;
+      4'b1011:  // >r
+                s_return        <= C_RETURN_INC;
+                s_bus_r         <= C_BUS_R_T;
+                s_bus_t         <= C_BUS_T_PRE;
+                s_bus_n         <= C_BUS_N_STACK;
+                s_stack         <= C_STACK_DEC;
+      4'b1100:  // @ (fetch)
+                s_bus_t         <= C_BUS_T_MEMORY;
+      4'b1101:  // ! (store)
+                s_bus_t         <= C_BUS_T_PRE;
+                s_bus_n         <= C_BUS_N_STACK;
+                s_stack         <= C_STACK_DEC;
+      4'b1110:  // r> (pop the return stack and push it onto the data stack)
+                s_return        <= C_RETURN_DEC;
                 s_bus_t         <= C_BUS_T_PRE;
                 s_bus_n         <= C_BUS_N_T;
-                if (opcode_curr[0])
-                  s_stack         <= C_RETURN_DEC;
-      4'bXXXX:  // enable/disable the interrupt
+                s_stack         <= C_STACK_INC;
+      4'b1111:  // enable/disable the interrupt
                 s_interrupt_enabled_change <= 1'b1;
                 s_interrupt_enabled_next <= opcode_curr[0];
-      4'bXXXX:  // call
-                s_bus_pc        <= C_BUS_PC_JUMP;
-                s_bus_r         <= C_BUS_R_PC;
-      4'bXXXX:  // returnc
-                ?
-      4'bXXXX:  // reset
-                ?
-      4'bXXXX:  // over
-                ?
       default:  // nop
     endcase
   end
