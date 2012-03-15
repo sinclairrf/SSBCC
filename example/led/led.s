2532a6e434b8863e59a5ab37cc8caae8edad2077
Rodney Sinclair <rsinclair@sinclairrf.com>
2012-03-15 07:01:04 -0700

    Fix bugs

--- example/led/led.s
+++ example/led/led.s
@@ -1,15 +1,15 @@
-\ Copyright 2012, Sinclair R.F., Inc.
+; Copyright 2012, Sinclair R.F., Inc.
 
-\ Example LED flasher using 8-bit data
+; Example LED flasher using 8-bit data
 
-\ loop for 256*n clock cycles
+; consume for 256*6+3 clock cycles
 :pause ( - )
-  0 :l00 1 - dup 0= .returnc .jump(l00)
+  0 :l00 1 - dup .jumpc(l00) drop .return
 
-\ repeat "pause" 256 times
+; repeat "pause" 256 times
 :repause
-  0 :l01 .call(pause) 1 - dup 0= .returnc .jump(l01)
+  0 :l01 .call(pause) 1 - dup .jumpc(l01) drop .return
 
-\ main program (as an infinite loop)
-\ T is the setting for the LED
+; main program (as an infinite loop)
+; T is the setting for the LED
 0 :l02 1 xor dup C_LED out .call(repause) .jump(l02)
