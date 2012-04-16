56ba9560714ccdc3aad67a8eb76a139fd048cc5c
Rodney Sinclair <rodney@sinclairrf.com>
2012-04-16 13:26:20 -0700

    Split the code generation into a separate module

--- ssbccGenVerilog.py
+++ ssbccGenVerilog.py
@@ -6,7 +6,72 @@
 #
 ################################################################################
 
+import re
+
 from ssbccUtil import *;
+
+def genInports(fp,inport):
+  if not inport['config']:
+    fp.write('// no input ports\n');
+    return
+  if inport['haveBitSignals']:
+    fp.write('always @ (*)\n');
+    fp.write('  case (s_T)\n');
+    for ix in range(len(inport['config'])):
+      configs = inport['config'][ix];
+      names = inport['name'][ix];
+      bitsString = '';
+      nBits = 0;
+      for jx in range(len(configs)):
+        if re.match(r'\d+-bit',configs[jx]):
+          a = re.findall(r'(\d+)',configs[jx]);
+          portLength = int(a[0]);
+          if len(bitsString) != 0:
+            bitsString = bitsString + ', ';
+          bitsString = bitsString + inport['name'][ix][jx];
+          nBits = nBits + portLength;
+      if nBits == 0:
+        pass;
+      elif nBits < 8:
+        fp.write('      8\'h%02X : s_T_inport = { %d\'h0, %s };\n' % (ix,8-nBits,bitsString));
+      elif nBits == 8:
+        fp.write('      8\'h%02X : s_T_inport = %s;\n' % (ix,bitsString));
+      else:
+        raise Exception('Too many bits in %s' % (inport['id'][ix]));
+    fp.write('    default : s_T_inport = 8\'h00;\n');
+    fp.write('  endcase\n');
+    fp.write('\n');
+  for ix in range(len(inport['config'])):
+    configs = inport['config'][ix];
+    names = inport['name'][ix];
+    for jx in range(len(configs)):
+      if re.match(r'\d+-bit',configs[jx]):
+        pass;
+      elif configs[jx] == 'strobe':
+        fp.write('initial %s = 1\'b0;\n' % names[jx]);
+        fp.write('always @ (posedge i_clk)\n');
+        fp.write('  if (i_rst)\n');
+        fp.write('    %s <= 1\'b0;\n' % names[jx]);
+        fp.write('  else if (s_inport)\n');
+        fp.write('    %s <= (s_T == 8\'h%02X);\n' % (names[jx],ix));
+        fp.write('  else\n');
+        fp.write('    %s <= 1\'b0;\n' % names[jx]);
+        fp.write('\n');
+      else:
+        raise Exception('Unrecognized INPORT type: "%s"' % configs[jx]);
+
+def genInstructions(fp,programBody,nInstructions):
+  fp.write('initial begin\n');
+  programBodyIx = 0;
+  for ix in range(len(programBody)):
+    if programBody[ix][0] == '-':
+      fp.write('  // %s\n' % programBody[ix][2:]);
+    else:
+      fp.write('  s_opcodeMemory[\'h%X] = 9\'h%s; // %s\n' % (programBodyIx,programBody[ix][0:3],programBody[ix][4:]));
+      programBodyIx = programBodyIx + 1;
+  for ix in range(programBodyIx,nInstructions):
+    fp.write('  s_opcodeMemory[\'h%X] = 9\'h000;\n' % ix);
+  fp.write('end\n');
 
 # TODO -- accommodate m*n architecture statements
 def genMemory(fp,memories):
@@ -61,3 +126,93 @@ def genMemory(fp,memories):
         fp.write('    2\'d%d : s_memory <= s_mem%d_out;\n' % (ixBank,ixBank));
     fp.write('    default : s_memory <= 8\'h00;\n');
     fp.write('  endcase\n');
+
+def genModule(fp,outCoreName,inport,outport):
+  fp.write('module %s(\n' % outCoreName);
+  fp.write('  // synchronous reset and processor clock\n');
+  fp.write('  input  wire           i_rst,\n');
+  fp.write('  input  wire           i_clk');
+  if inport['config']:
+    fp.write(',\n');
+    fp.write('  // inport ports\n');
+    for ix in range(len(inport['config'])):
+      configs = inport['config'][ix];
+      names = inport['name'][ix];
+      for jx in range(len(configs)):
+        if (ix != 0) or (jx != 0):
+          fp.write(',\n');
+        if re.match(r'\d+-bit',configs[jx]):
+          inport['haveBitSignals'] = True;
+          a = re.findall(r'(\d+)',configs[jx]);
+          portLength = int(a[0]);
+          if portLength == 1:
+            fp.write('  input  wire           %s' % names[jx]);
+          elif portLength < 10:
+            fp.write('  input  wire     [%d:0] %s' % (portLength-1,names[jx]));
+          else:
+            fp.write('  input  wire    [%d:0] %s' % (portLength-1,names[jx]));
+        elif configs[jx] == 'strobe':
+          fp.write('  output reg            %s' % names[jx]);
+        else:
+          raise Exception('Unrecognized INPORT type: "%s"' % configs[jx]);
+  if outport['config']:
+    fp.write(',\n');
+    fp.write('  // outport ports\n');
+    for ix in range(len(outport['config'])):
+      configs = outport['config'][ix];
+      names = outport['name'][ix];
+      for jx in range(len(configs)):
+        if (ix != 0) or (jx != 0):
+          fp.write(',\n');
+        if re.match(r'\d+-bit',configs[jx]):
+          a = re.findall(r'(\d+)',configs[jx]);
+          portLength = int(a[0]);
+          if portLength == 1:
+            fp.write('  output reg            %s' % names[jx]);
+          elif portLength < 10:
+            fp.write('  output reg      [%d:0] %s' % (portLength-1,names[jx]));
+          else:
+            fp.write('  output reg     [%d:0] %s' % (portLength-1,names[jx]));
+        elif configs[jx] == 'strobe':
+          fp.write('  output reg            %s' % names[jx]);
+        else:
+          raise Exception('Unrecognized INPORT type: "%s"' % configs[jx]);
+  fp.write('\n');
+  fp.write(');\n');
+
+def genOutports(fp,outport):
+  if not outport['config']:
+    fp.write('// no output ports\n');
+    return;
+  for ix in range(len(outport['config'])):
+    configs = outport['config'][ix];
+    names = outport['name'][ix];
+    for jx in range(len(configs)):
+      if re.match(r'\d+-bit',configs[jx]):
+        a = re.findall(r'(\d+)',configs[jx]);
+        portLength = int(a[0]);
+        fp.write('initial %s = %d\'h0;\n' % (names[jx],portLength));
+        fp.write('always @ (posedge i_clk)\n');
+        fp.write('  if (i_rst)\n');
+        fp.write('    %s <= %d\'h0;\n' % (names[jx],portLength));
+        fp.write('  else if (s_outport && (s_T == 8\'h%02X))\n' % ix);
+        fp.write('    %s <= s_N[0+:%d];\n' % (names[jx],portLength));
+        fp.write('  else\n');
+        fp.write('    %s <= %s;\n' % (names[jx],names[jx]));
+        fp.write('\n');
+      elif configs[jx] == 'strobe':
+        fp.write('initial %s = 1\'b0;\n' % names[jx]);
+        fp.write('always @ (posedge i_clk)\n');
+        fp.write('  if (i_rst)\n');
+        fp.write('    %s <= 1\'b0;\n' % names[jx]);
+        fp.write('  else if (s_outport)\n');
+        fp.write('    %s <= (s_T == 8\'h%02X);\n' % (names[jx],ix));
+        fp.write('  else\n');
+        fp.write('    %s <= 1\'b0;\n' % names[jx]);
+        fp.write('\n');
+      else:
+        raise Exception('Unrecognized OUTPORT type: "%s"' % configs[jx]);
+
+def genUserHeader(fp,header):
+  for ix in range(len(user_header)):
+    fpOutCore.write('// %s\n' % user_header[ix]);
