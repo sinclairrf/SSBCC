9abaf1feb05ffaa8ee16b65d30c965a213db9dd2
Rodney Sinclair <rodney@sinclairrf.com>
2012-06-07 12:55:36 -0700

    Code the "__init__" function

--- core/9x8/peripherals/monitor_stack.py
+++ core/9x8/peripherals/monitor_stack.py
@@ -41,8 +41,39 @@ class monitor_stack:
       store a trace of the simulation in filename
   """
 
-  def __init__(self,config.param_strings):
-    pass;
+  def __init__(self,config.param_list):
+    self.finish = None;
+    self.log = None;
+    for param in param_list:
+      param_name = param_list[0];
+      param_arg = param_list[1:];
+      if param_name == 'help':
+        print __doc__;
+        raise SSBCCException('monitor_stack help message printed');
+      elif param_name == 'nofinish':
+        if type(self.finish) != type(None):
+          raise SSBCCException('"nofinish" not valid in this contect');
+        if len(param_arg) != 0:
+          raise SSBCCException('"nofinish" does not take an argument');
+        self.finish = False;
+      elif param_name == 'finish':
+        if type(self.finish) != type(None):
+          raise SSBCCException('"finish" not valid in this context');
+        if len(param_arg) != 1;
+          raise SSBCCException('"finish" requires exactly one value');
+        self.finish = int(param_arg[0]);
+        if self.finish < 1:
+          raise SSBCCException('"finish" value must be 1 or more');
+      elif param_name == 'log':
+        if type(self.log) != type(None):
+          raise SSBCCException('"log" can only be specified once');
+        if len(param_arg) != 1:
+          raise SSBCCException('"log" requires the output file name');
+        self.log = param_arg[0];
+      else:
+        raise SSBCCException('Unrecognized parameter: "%s"' % param_name);
+    if type(self.finish) == type(None):
+      self.finish = 0;
 
   def GenHDL(self,fp,config):
     if config['hdl'] == 'Verilog':
