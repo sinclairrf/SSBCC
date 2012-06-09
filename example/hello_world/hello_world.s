81bad5e1c34264d87363659c22640058821ac5f3
Rodney Sinclair <rsinclair@sinclairrf.com>
2012-06-09 09:11:49 -0700

    Clean up stack at end of transmission

--- example/hello_world/hello_world.s
+++ example/hello_world/hello_world.s
@@ -9,7 +9,7 @@
 .main
 
   C"Hello World\r\n"
-  :loop 1- swap .outport(O_UART_TX) :wait .inport(I_UART_TX_BUSY) .jumpc(wait) .jumpc(loop,nop)
+  :loop 1- swap .outport(O_UART_TX) :wait .inport(I_UART_TX_BUSY) .jumpc(wait) .jumpc(loop,nop) drop
 
 ; Wait forever after the message is printed.
 :forever .jump(forever)
