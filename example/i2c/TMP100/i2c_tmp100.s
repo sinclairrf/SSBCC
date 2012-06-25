80cbead5057ef565fa5a9651e7f2bfe6cf2e6dfa
Rodney Sinclair <rsinclair@sinclairrf.com>
2012-06-25 16:39:53 -0700

    Get working with a single TMP100 temperature sensor

--- example/i2c/TMP100/i2c_tmp100.s
+++ example/i2c/TMP100/i2c_tmp100.s
@@ -10,23 +10,31 @@
 .main
 
   :infinite
+    "\r\n\0"
     .call(get_i2c_temp) .jumpc(error)
-      .call(print_2bytes)
+      >r .call(byte_to_hex) r> .call(byte_to_hex)
+      .jump(no_error)
     :error
+      "N/A "
+    :no_error
+    :print_loop
+      .outport(O_UART_TX)
+      :print_wait .inport(I_UART_TX) .jumpc(print_wait)
+      .jumpc(print_loop,nop) drop
     .call(wait_1_sec)
   .jump(infinite)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-; Wait one second with a 97 MHz clock.
+; Wait one second with a 100 MHz clock.
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .function wait_1_sec
 
-  ; 97 iterations
-  ${97-1} :outer
+  ; 100 iterations
+  ${100-1} :outer
     ; 1000 iterations
     ${4-1} :mid_outer
       ${250-1} :mid_inner
@@ -54,34 +62,6 @@
   drop .call(i2c_read_byte) >r .call(i2c_read_byte) r> 0
   :error
   .call(i2c_send_stop)
-.return
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; Print the 2-byte temperature as a hex value.
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-.function print_2bytes
-
-  ; construct the message as a null-terminated string:
-  ;   put the two bytes to send onto the stack (MSB first, LSB second)
-  ;   end each line with a carriage-return, line-feed pair
-  ;   convert the LSB from a single-byte to two hex bytes
-  ;   convert the MSB from a single-byte to two hex bytes
-  ;   add the length of the message
-  ; ( u_LSB u_MSB - '\0' '\n' '\r' p0 p1 p2 p3 )
-  >r >r
-  N"\r\n"
-  r> .call(byte_to_hex)
-  r> .call(byte_to_hex)
-
-  ; transmit the message
-  :loop .outport(O_UART_TX)
-    :wait .inport(I_UART_TX) .jumpc(wait)
-  .jumpc(loop,nop) drop
-
 .return
 
 
