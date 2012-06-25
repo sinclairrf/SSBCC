cc99bcb1bc63bccbf71d7159730f1ea318cf4961
Rodney Sinclair <rsinclair@sinclairrf.com>
2012-06-25 08:52:18 -0700

    Add printout of deserialized UART output

--- example/hello_world/tb.v
+++ example/hello_world/tb.v
@@ -22,13 +22,32 @@ initial begin
   $finish;
 end
 
+wire s_UART_Tx;
 hello_world uut(
   // synchronous reset and processor clock
   .i_rst        (s_rst),
   .i_clk        (s_clk),
   // outport ports
-  .o_UART_Tx    ()
+  .o_UART_Tx    (s_UART_Tx)
 );
+
+localparam baud = 115200;
+localparam dt_baud = 1.0e9/baud;
+reg [8:0] deser = 9'h1FF;
+initial forever begin
+  @ (negedge s_UART_Tx);
+  #(dt_baud/2.0);
+  repeat (9) begin
+    #dt_baud;
+    deser = { s_UART_Tx, deser[1+:8] };
+  end
+  if (deser[8] != 1'b1)
+    $display("%13d : Malformed UART transmition, $time");
+  else if ((8'h20 <= deser[0+:8]) && (deser[0+:8]<=8'h80))
+    $display("%13d : Sent 0x%02H : %c", $time, deser[0+:8], deser[0+:8]);
+  else
+    $display("%13d : Sent 0x%02H", $time, deser[0+:8]);
+end
 
 initial begin
   $dumpfile("tb.vcd");
