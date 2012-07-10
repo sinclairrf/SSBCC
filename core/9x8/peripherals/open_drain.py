457958ea260ea5e8d47b285d193e32b9d13b9035
Rodney Sinclair <rsinclair@sinclairrf.com>
2012-07-10 08:15:59 -0700

    Start adding base class for peripherals to facilitate writing periperals

--- core/9x8/peripherals/open_drain.py
+++ core/9x8/peripherals/open_drain.py
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-class open_drain:
+from ssbccPeripheral import SSBCCperipheral
+
+class open_drain(SSBCCperipheral):
   """Implement an open-drain IOB suitable for direct connection to a pin.  This can,
 for example, be used as an I/O port for an I2C device.
 
@@ -49,60 +51,34 @@ Example:  Configure two 1-bit ports implementing an I2C bus:
 """
 
   def __init__(self,config,param_list,ixLine):
-    # Get the parameters.
-    self.inport = None;
-    self.iosignal = None;
-    self.outport = None;
-    self.width = None;
+    # Parse the parameters.
     for param_tuple in param_list:
       param = param_tuple[0];
       param_arg = param_tuple[1];
       if param == 'inport':
-        if self.inport != None:
-          raise SSBCCException('inport can only be specified once at line %d' % ixLine);
-        if param_arg == None:
-          raise SSBCCException('inport assignment missing at line %d' % ixLine);
-        if not re.match('I_\w+$',param_arg):
-          raise SSBCCException('Bad inport symbol at line %d:  "%s"' % (ixLine,param_arg,));
-        self.inport = param_arg;
+        self.AddAttr(config,'inport',param_arg,r'I_\w+$',ixLine);
       elif param == 'iosignal':
-        if self.iosignal != None:
-          raise SSBCCException('iosignal can only be specified once at line %d' % ixLine);
-        if param_arg == None:
-          raise SSBCCException('iosignal assignment missing at line %d' % ixLine);
-        if not re.match('io_\w+$',param_arg):
-          raise SSBCCException('Bad io signal name at line %d:  "%s"' % (ixLine,param_arg,));
-        self.iosignal = param_arg;
+        self.AddAttr(config,'iosignal',param_arg,r'io_\w+$',ixLine);
       elif param == 'outport':
-        if self.outport != None:
-          raise SSBCCException('outport can only be specified once at line %d' % ixLine);
-        if param_arg == None:
-          raise SSBCCException('outport assignment missing at line %d' % ixLine);
-        if not re.match('O_\w+$',param_arg):
-          raise SSBCCException('Bad outport symbol at line %d:  "%s"' % (ixLine,param_arg,));
-        self.outport = param_arg;
+        self.AddAttr(config,'outport',param_arg,r'O_\w+$',ixLine);
       elif param == 'width':
-        if self.width != None:
-          raise SSBCCException('width can only be specified once at line %d' % ixLine);
-        if param_arg == None:
-          raise SSBCCException('width assignment missing at line %d' % ixLine);
-        if not re.match('[1-9][0-9]*$',param_arg):
-          raise SSBCCException('Bad signal width at line %d:  "%s"' % (ixLine,param_arg,));
-        self.width = int(param_arg);
+        self.AddAttr(config,'width',param,r'[1-9][0-9]*$',ixLine);
+        self.width = int(self.width);
       else:
         raise SSBCCException('Unrecognized parameter at line %d:  "%s"' % (ixLine,param,));
     # Set defaults for non-specified values.
-    if self.inport == None:
+    if not hasattr(self,'inport'):
       raise SSBCCException('Missing "inport=I_name" at line %d' % ixLine);
-    if self.iosignal == None:
+    if not hasattr(self,'iosignal'):
       raise SSBCCException('Missing "iosignal=io_name" at line %d' % ixLine);
-    if self.outport == None:
+    if not hasattr(self,'outport'):
       raise SSBCCException('Missing "outport=O_name" at line %d' % ixLine);
-    if self.width == None:
+    if not hasattr(self,'width'):
       self.width = 1;
     # Ensure the speicified values are reasonable.
-    if (self.width < 1) or (8 < self.width):
-      raise SSBCCException('pull-down peripheral width must be between 1 and 8 inclusive');
+    maxWidth = config.Get('data_width');
+    if (self.width < 1) or (maxWidth < self.width):
+      raise SSBCCException('width must be between 1 and %d inclusive at line %d' % (maxWidth,ixLine,));
     # Add the I/O port and OUTPORT and INPORT signals for this peripheral.
     self.sname = 's__' + self.iosignal;
     sname_init = '%d\'b%s' % (self.width, '1'*self.width, );
@@ -114,12 +90,6 @@ Example:  Configure two 1-bit ports implementing an I2C bus:
     config.AddOutport((self.outport,
                       (self.sname,self.width,'data',sname_init,),
                      ));
-
-  def GenHDL(self,fp,config):
-    if config.Get('hdl') == 'Verilog':
-      self.GenVerilog(fp,config);
-    else:
-      raise Exception('HDL "%s" not implemented' % config.Get('hdl'));
 
   def GenVerilog(self,fp,config):
     body_1 = """//
---
+++ ssbccPeripheral.py
@@ -0,0 +1,38 @@
+################################################################################
+#
+# Copyright 2012, Sinclair R.F., Inc.
+#
+################################################################################
+
+import re
+
+from ssbccUtil import SSBCCException
+
+class SSBCCperipheral:
+  """Base class for peripherals"""
+
+  def __init__(self,config,param_list,ixLine):
+    pass;
+
+  def AddAttr(self,config,name,value,reformat,ixLine):
+    if hasattr(self,name):
+      raise SSBCCperipheral('%s repeated at line %d' % (name,ixLine,));
+    if value == None:
+      raise SSBCCperipheral('%s missing value at line %d' % (name,ixLine,));
+    if not re.match(reformat,value):
+      raise SSBCCException('Inport symbol at line %d does not match required format "%s":  "%s"' % (ixLine,reformat,value,));
+    setattr(self,name,value);
+
+  def GenHDL(self,fp,config):
+    if config.Get('hdl') == 'Verilog':
+      self.GenVerilog(fp,config);
+    elif config.Get('hdl') == 'VHDL':
+      self.GenVHDL(fp,config);
+    else:
+      raise SSBCCException('HDL "%s" not implemented' % config.Get('hdl'));
+
+  def GenVerilog(self,fp,config):
+    raise Exception('Verilog is not implemented for this peripheral');
+
+  def GenVHDL(self,fp,config):
+    raise Exception('VHDL is not implemented for this peripheral');
