72775157a52ff435983b779508952746ac3f9c84
Rodney Sinclair <rodney@sinclairrf.com>
2013-02-12 16:45:55 -0800

    Document base class for peripherals

--- ssbccPeripheral.py
+++ ssbccPeripheral.py
@@ -25,6 +25,15 @@ class SSBCCperipheral:
     pass;
 
   def AddAttr(self,config,name,value,reformat,ixLine):
+    """
+    Add attribute to the peripheral:
+    config      ssbccConfig object for the procedssor core
+    name        attribute name
+    value       possibly optional value for the attribute
+    reformat    regular expression format for the attribute value
+                Note:  reformat=None means the attribute can only be set to True
+    ixLine      line number for the peripheral for error messages
+    """
     if hasattr(self,name):
       raise SSBCCException('%s repeated at line %d' % (name,ixLine,));
     if reformat == None:
@@ -39,9 +48,17 @@ class SSBCCperipheral:
       setattr(self,name,value);
 
   def GenAssembly(self,config):
+    """
+    Used to generate any assembly modules associated with the peripheral.
+    """
     pass;
 
   def GenHDL(self,fp,config):
+    """
+    Generate the peripheral HDL.
+    fp          file pointer for the output processor
+    config      ssbccConfig object for the procedssor core
+    """
     if config.Get('hdl') == 'Verilog':
       self.GenVerilog(fp,config);
     elif config.Get('hdl') == 'VHDL':
@@ -50,23 +67,45 @@ class SSBCCperipheral:
       raise SSBCCException('HDL "%s" not implemented' % config.Get('hdl'));
 
   def GenVerilog(self,fp,config):
+    """
+    Generate the Verilog version of the peripheral.
+    Raise an exception if there is no Verilog version of the peripheral.
+    """
     raise Exception('Verilog is not implemented for this peripheral');
 
   def GenVerilogFinal(self,config,body):
+    """
+    Clean up the peripheral code.
+    Change "$clog2" to "clog2" for simulators and synthesis tools that don't
+      recognize or process "$clog2."
+    """
     if config.Get('define_clog2'):
       body = re.sub('\$clog2','clog2',body);
     return body;
 
   def GenVHDL(self,fp,config):
+    """
+    Generate the VHDL version of the peripheral.
+    Raise an exception if there is no VHDL version of the peripheral.
+    """
     raise Exception('VHDL is not implemented for this peripheral');
 
   def IsInt(self,value):
+    """
+    Test the string to see if it is a well-formatted integer.
+    Allow underscores as per Verilog.
+    """
     if re.match(r'[1-9][0-9_]*$',value):
       return True;
     else:
       return False;
 
   def IsIntExpr(self,value):
+    """
+    Test the string to see if it is a well-formatted integer or multiplication
+    of two integers.
+    Allow underscores as per Verilog.
+    """
     if re.match(r'[1-9][0-9_]*',value):
       return True;
     elif re.match(r'\([1-9][0-9_]*(\*[1-9][0-9_]*)+\)',value):
@@ -75,14 +114,45 @@ class SSBCCperipheral:
       return False;
 
   def IsParameter(self,config,name):
+    """
+    See if the provided symbol name is a parameter in the processor core.
+    config      ssbccConfig object for the procedssor core
+    name        symbol name
+    """
     return config.IsParameter(name);
 
+  def LoadCore(self,filename,extension):
+    """
+    Read the source HDL for the peripheral from the same directory as the python
+    script.
+    filename    name for the python peripheral (usually "__file__")
+    extension   the string such as ".v" or ".vhd" required by the HDL
+    """
+    hdlName = re.sub(r'\.py$',extension,filename);
+    fp = open(hdlName,'r');
+    body = fp.read();
+    fp.close();
+    return body;
+
   def ParseInt(self,value):
+    """
+    Convert a well-formatted integer string to an integer.
+    Allow underscores as per Verilog.
+    Note:  If this routine is called, then the value should have already been
+           verified to be a well-formatted integer string.
+    """
     if not self.IsInt(value):
       raise Exception('Program Bug -- shouldn\'t call with a badly formatted integer');
     return int(re.sub('_','',value));
 
   def ParseIntExpr(self,value):
+    """
+    Convert a string containing well-formatted integer or multiplication of two
+    integers.
+    Allow underscores as per Verilog.
+    Note:  If this routine is called, then the value should have already been
+           verified to be a well-formatted integer string.
+    """
     if not self.IsIntExpr(value):
       raise Exception('Program Bug -- shouldn\'t call with a badly formatted integer expression');
     return eval(re.sub('_','',value));
