5fd91d07376f09e9c88c4a1baf54ab4587487f50
Rodney Sinclair <rsinclair@sinclairrf.com>
2013-04-09 15:40:39 -0700

    Add UART_Rx peripheral

---
+++ core/9x8/peripherals/UART_Rx.py
@@ -0,0 +1,229 @@
+################################################################################
+#
+# Copyright 2013, Sinclair R.F., Inc.
+#
+################################################################################
+
+import math;
+import re;
+
+from ssbccPeripheral import SSBCCperipheral
+from ssbccUtil import SSBCCException;
+
+class UART_Rx(SSBCCperipheral):
+  """
+  Receive UART:
+    1 start bit
+    8 data bits
+    1 or 2 stop bits\n
+  Usage:
+    PERIPHERAL UART inport=I_inport_name        \\
+                    inempty=I_inempty_name      \\
+                    inerror=I_inerror_name      \\
+                    baudmethod={clk/rate|count} \\
+                    [insignal=i_name]           \\
+                    [noSync|sync=n]             \\
+                    [noDeglitch|deglitch=n]     \\
+                    [noInFIFO|inFIFO=n]         \\
+                    [nStop={1|2}]               \\             
+                    [edgetol=x]                 \n
+  Where:
+    inport=I_inport_name
+      specifies the symbol used by the inport instruction to read a received by
+      from the peripheral
+      Note:  The name must start with "I_".
+    inempty=I_inempty_name
+      specifies the symbol used by the inport instruction to get the empty
+      status of the input side of the peripheral
+      Note:  The name must start with "I_".
+    inerror=I_inerror_name
+      specified the symbol used by the inport instruction to get the error
+      status of the input side of the peripheral
+      Note:  The name must start with "I_".
+      Note:  The 3 bits in this value have the following meaning:
+             bit 0 -- a read was performed when no data was available or a new
+                      byte of data was received and the input buffer or FIFO was
+                      already full
+                      Note:  This bit indicates that an error has occured and
+                             one or more bytes is missing in the data stream.
+             bit 1 -- the received edge rate does not match the commanded edge
+                      rate, clock, and tolerance
+                      Note:  This error is cleared by an idle input stream.
+             bit 2 -- missing start bit or stop bit(s) in data stream
+                      Note:  This error is cleared by an idle input stream.
+    baudmethod
+      specifies the method to generate the desired bit rate:
+      1st method:  clk/rate
+        clk is the frequency of "i_clk" in Hz
+          a number will be interpreted as the clock frequency in Hz
+          a symbol will be interpreted as a parameter
+            Note:  this parameter must have been declared with a "PARAMETER"
+            command
+        rate is the desired baud rate
+          this is specified as per "clk"
+      2nd method:
+        specify the number of "i_clk" clock cycles between bit edges
+      Note:  clk, rate, and count can be parameters or constants.  For example,
+             the following uses the parameter G_CLK_FREQ_HZ for the clock
+             frequency and a hard-wired baud rate of 9600:
+             "baudmethod=G_CLK_FREQ_HZ/9600".
+      Note:  The numeric values can have Verilog-style '_' separators between
+               the digits.  For example, 100_000_000 represents 100 million.
+    insignal=i_name
+      optionally specifies the name of the single-bit transmit signal
+      Default:  i_UART_Rx
+    noSync
+      optionally state no synchronization or registration is performed on the
+      input signal.
+    sync=n
+      optionally state that an n-bit synchronizer will be performed on the
+      input signal.
+      Note:  sync=3 is the default.
+    noDeglitch
+      optionally state that no deglitching is performed on the input signal.
+      Note:  This is the defalt.
+    deglitching=n
+      optionally state that an n-bit deglitcher is performed on the input signal
+      Note:  Deglitching consists of changing the output state when n
+             successive input bits are in the opposite state.
+    noInFIFO
+      optionally state that the peripheral will not have an input FIFO
+      Note:  This is the default.
+    inFIFO=n
+      optionally add a FIFO of depth n to the input side of the UART
+      Note:  n must be a power of 2.
+    nStop=n
+      optionally configure the peripheral for n stop bits
+      default:  1 stop bit
+      Note:  n must be 1 or 2
+      Note:  the peripheral does not accept 1.5 stop bits
+    edgetol=x
+      optionally specify the tolerance for edge detection/generation for the
+      deserializer as x%
+      Note:  The default is 2.5%
+      Note:  The tolerance is cumulative as edges are missed, so a tolerance
+             that accumulates to 50% or more in 9 edges, i.e., about 5.5%, is
+             excessive.  A reasonable limit is about half of that, i.e., about
+             2.5%, so that is what is specified.  Smaller tolerances required
+             the recevied signal and the edge generating using the provided
+             micro controller clock to more closely match the nominal baud rate.\n
+      Note:  The tolerance is limited to be between 0.1 and 5.5 and may be
+             expressed as an integer or as a fraction with a single digit after
+             the decimal.
+  The following ports are provided by this peripheral:
+    I_inport_name
+      input a recieved byte from the peripheral
+      Note:  If there is no input FIFO, then this is the last received byte.
+             If there is an input FIFO, then this is the next byte in the FIFO.
+      Note:  If there is an input FIFO and the read would cause a FIFO
+             underflow, this will repeat the last received byte.
+    I_inempty_name\n
+      input the status of the input side of the peripheral
+      bit 0:  input empty
+        this bit will be high when the input side of the peripheral has one or
+        more bytes read to be read
+        Note:  If there is no FIFO this means that a single byte is ready to be
+               read and has not been read.  If there is an input FIFO this
+               means that there are one or more bytes in the FIFO.
+        Note:  "Empty" is used rather than "ready" to facilitate loops that
+               respond when there is a new byte ready to be processed.  See the
+               examples below.
+  """
+
+  def __init__(self,peripheralFile,config,param_list,ixLine):
+    # Use the externally provided file name for the peripheral
+    self.peripheralFile = peripheralFile;
+    # Get the parameters.
+    for param_tuple in param_list:
+      param = param_tuple[0];
+      param_arg = param_tuple[1];
+      for param_test in (
+          ('deglitch',   r'[1-9]\d*$', int,   ),
+          ('edgetol',    r'(0\.[1-9]|[1-4](\.\d)?|5(\.[0-5])?)$',
+                                       float, ),
+          ('inempty',    r'I_\w+$',    None,  ),
+          ('inerror',    r'I_\w+$',    None,  ),
+          ('inport',     r'I_\w+$',    None,  ),
+          ('insignal',   r'i_\w+$',    None,  ),
+          ('noDeglitch', None,         None,  ),
+          ('noInFIFO',   None,         None,  ),
+          ('noSync',     None,         None,  ),
+          ('nStop',      r'[12]$',     int,   ),
+          ('sync',       r'[1-9]\d*$', int,   ),
+        ):
+        if param == param_test[0]:
+          self.AddAttr(config,param,param_arg,param_test[1],ixLine,param_test[2]);
+          break;
+      else:
+        if param == 'baudmethod':
+          self.AddRateMethod(config,param,param_arg,ixLine);
+        elif param in ('inFIFO',):
+          self.AddAttr(config,param,param_arg,r'[1-9]\d*$',ixLine,int);
+          x = getattr(self,param);
+          if math.modf(math.log(x,2))[0] != 0:
+            raise SSBCCException('%s=%d must be a power of 2 at line %d' % (param,x,ixLine,));
+        else:
+          raise SSBCCException('Unrecognized parameter at line %d: %s' % (ixLine,param,));
+    # Ensure the required parameters are provided.
+    for paramname in ('baudmethod','inempty','inerror','inport',):
+      if not hasattr(self,paramname):
+        raise SSBCCException('Required parameter "%s" is missing at line %d' % (paramname,ixLine,));
+    # Set optional parameters.
+    for optionalpair in ( ('edgetol',2.5), ('nStop',1,), ('insignal','i_UART_Rx',), ):
+      if not hasattr(self,optionalpair[0]):
+        setattr(self,optionalpair[0],optionalpair[1]);
+    # Ensure exclusive pair configurations are set and consistent.
+    for exclusivepair in (
+        ('noSync',     'sync',     'sync',       3,    ),
+        ('noDeglitch', 'deglitch', 'noDeglitch', True, ),
+        ('noInFIFO',   'inFIFO',   'noInFIFO',   True, ),
+      ):
+      if hasattr(self,exclusivepair[0]) and hasattr(self,exclusivepair[1]):
+        raise SSBCCException('Only one of "%s" and "%s" can be specified at line %d' % (exclusivepair[0],exclusivepair[1],ixLine,));
+      if not hasattr(self,exclusivepair[0]) and not hasattr(self,exclusivepair[1]):
+        setattr(self,exclusivepair[2],exclusivepair[3]);
+      if hasattr(self,exclusivepair[0]):
+        delattr(self,exclusivepair[0]);
+        setattr(self,exclusivepair[1],0);
+    # Set the string used to identify signals associated with this peripheral.
+    self.namestring = self.insignal;
+    # Add the I/O port, internal signals, and the INPORT and OUTPORT symbols for this peripheral.
+    config.AddIO(self.insignal,1,'input',ixLine);
+    config.AddSignal('s__%s__Rx'          % self.namestring,8,ixLine);
+    config.AddSignal('s__%s__Rx_empty'    % self.namestring,1,ixLine);
+    config.AddSignal('s__%s__Rx_error'    % self.namestring,3,ixLine);
+    config.AddSignal('s__%s__Rx_error_rd' % self.namestring,1,ixLine);
+    config.AddSignal('s__%s__Rx_rd'       % self.namestring,1,ixLine);
+    config.AddInport((self.inport,
+                    ('s__%s__Rx'          % self.namestring,8,'data',),
+                    ('s__%s__Rx_rd'       % self.namestring,1,'strobe',),
+                   ),ixLine);
+    config.AddInport((self.inempty,
+                   ('s__%s__Rx_empty'     % self.namestring,1,'data',),
+                  ),ixLine);
+    config.AddInport((self.inerror,
+                   ('s__%s__Rx_error'     % self.namestring,3,'data',),
+                   ('s__%s__Rx_error_rd'  % self.namestring,1,'strobe',),
+                  ),ixLine);
+    # Add the 'clog2' function to the processor (if required).
+    config.functions['clog2'] = True;
+
+  def GenVerilog(self,fp,config):
+    for bodyextension in ('.v',):
+      body = self.LoadCore(self.peripheralFile,bodyextension);
+      for subpair in (
+                    (r'\bL__',          'L__@NAME@__', ),
+                    (r'\bgen__',        'gen__@NAME@__', ),
+                    (r'\bs__',          's__@NAME@__', ),
+                    (r'@INPORT@',       self.insignal, ),
+                    (r'@BAUDMETHOD@',   str(self.baudmethod), ),
+                    (r'@EDGETOL@',      str(int(10*self.edgetol)), ),
+                    (r'@SYNC@',         str(self.sync), ),
+                    (r'@DEGLITCH@',     str(self.deglitch), ),
+                    (r'@INFIFO@',       str(self.inFIFO), ),
+                    (r'@NSTOP@',        str(self.nStop), ),
+                    (r'@NAME@',         self.namestring, ),
+                  ):
+        body = re.sub(subpair[0],subpair[1],body);
+      body = self.GenVerilogFinal(config,body);
+      fp.write(body);
---
+++ core/9x8/peripherals/UART_Rx.v
@@ -0,0 +1,315 @@
+//
+// PERIPHERAL UART_Rx:  @NAME@
+//
+// Technique:
+// - optionally synchronize the incoming signal
+// - optionally deglitch the incoming signal
+// - identify edges, align with value before the edge
+// - generate missing edges, error if non-aligned edges, align values
+// - assemble received bit sequence
+// - run state machine counting number of received bits and waiting for delayed start bits
+// - validate bit sequence and output bit sequence at end of last stop bit
+// - optional FIFO
+//
+generate
+localparam L__BAUDMETHOD = @BAUDMETHOD@;
+localparam L__BAUDMETHOD_NBITS = $clog2(L__BAUDMETHOD);
+// Either copy the input, register it, or put it through a synchronizer.
+localparam L__SYNC_LENGTH = @SYNC@;
+wire s__Rx_sync;
+if (L__SYNC_LENGTH == 0) begin : gen__no_sync
+  assign s__Rx_sync = @INPORT@;
+end else if (L__SYNC_LENGTH == 1) begin : gen__short_sync
+  reg s__Rx_inport_s = 1'b1;
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_inport_s <= 1'b1;
+    else
+      s__Rx_inport_s <= @INPORT@;
+  assign s__Rx_sync = s__Rx_inport_s;
+end else begin : gen__long_sync
+  reg [L__SYNC_LENGTH-1:0] s__Rx_inport_s = {(L__SYNC_LENGTH){1'b1}};
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_inport_s <= {(L__SYNC_LENGTH){1'b1}};
+    else
+      s__Rx_inport_s <= { s__Rx_inport_s[0+:L__SYNC_LENGTH-1], @INPORT@ };
+  assign s__Rx_sync = s__Rx_inport_s[L__SYNC_LENGTH-1];
+end
+// Either pass the received signal with no deglitching or apply deglitch
+// hysteresis that consists of not changing the reported state unless all of
+// the queued bits have changed state.
+localparam L__DEGLITCH_LENGTH = @DEGLITCH@;
+reg s__Rx_deglitched;
+if (L__DEGLITCH_LENGTH == 0) begin : gen__nodeglitch
+  always @ (*)
+    s__Rx_deglitched <= s__Rx_sync;
+end else begin : gen__deglitch
+  initial s__Rx_deglitched = 1'b1;
+  reg [L__DEGLITCH_LENGTH-1:0] s__Rx_deglitch = {(L__DEGLITCH_LENGTH){1'b1}};
+  always @ (posedge i_clk) begin
+    s__Rx_deglitched <= (&(s__Rx_deglitch != {(L__DEGLITCH_LENGTH){s__Rx_deglitched}})) ? ~s__Rx_deglitched : s__Rx_deglitched;
+    s__Rx_deglitch <= { s__Rx_deglitch[0+:L__DEGLITCH_LENGTH-1], @INPORT@ };
+  end
+end
+// Identify idle state for error recovery.  This consists of 1+8+nStop bits of
+// all ones.
+localparam L__IDLE_LENGTH = (1+8+@NSTOP@)*L__BAUDMETHOD-1;
+localparam L__IDLE_LENGTH_NBITS = $clog2(L__IDLE_LENGTH+1);
+reg [L__IDLE_LENGTH_NBITS-1:0] s__Rx_input_idle_count = L__IDLE_LENGTH[L__IDLE_LENGTH_NBITS-1:0];
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_input_idle_count <= L__IDLE_LENGTH[L__IDLE_LENGTH_NBITS-1:0];
+  else if (s__Rx_deglitched == 1'b0)
+    s__Rx_input_idle_count <= L__IDLE_LENGTH[L__IDLE_LENGTH_NBITS-1:0];
+  else
+    s__Rx_input_idle_count <= s__Rx_input_idle_count - { {(L__IDLE_LENGTH_NBITS-1){1'b0}}, 1'b1 };
+reg s__Rx_input_idle = 1'b1;
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_input_idle <= 1'b1;
+  else if (s__Rx_deglitched == 1'b0)
+    s__Rx_input_idle <= 1'b0;
+  else if (s__Rx_input_idle_count == {(L__IDLE_LENGTH_NBITS){1'b0}})
+    s__Rx_input_idle <= 1'b1;
+  else
+    s__Rx_input_idle <= s__Rx_input_idle;
+// Identify edges
+reg s__Rx_last = 1'b1;
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_last <= 1'b1;
+  else
+    s__Rx_last <= s__Rx_deglitched;
+reg s__Rx_edge = 1'b0;
+reg s__Rx_edge_value = 1'b1;
+always @ (posedge i_clk)
+  if (i_rst) begin
+    s__Rx_edge <= 1'b0;
+    s__Rx_edge_value <= 1'b1;
+  end else begin
+    s__Rx_edge <= (s__Rx_deglitched != s__Rx_last);
+    s__Rx_edge_value <= s__Rx_last;
+  end
+// State machine signal -- waiting for first edge of start bit.
+reg s__Rx_waiting;
+// Run a timer to (1) capture values at the center of bits and (2) ensure edges
+// are consistently timed to a +/-2.5% tolerance.
+localparam L__EDGE_TOL          = @EDGETOL@;
+localparam L__EDGE_TIMER_SIZE   = ((1000+9*25)*L__BAUDMETHOD+500)/1000-1;
+localparam L__EDGE_TIMER_NBITS  = $clog2(L__EDGE_TIMER_SIZE+1);
+localparam L__EDGE_TIMER_START  = ((1000+L__EDGE_TOL)*L__BAUDMETHOD+500)/1000-1;
+localparam L__EDGE_TIMER_TOL    = (2*L__EDGE_TOL*L__BAUDMETHOD+500)/1000;
+reg [L__EDGE_TIMER_NBITS-1:0] s__Rx_edge_timer = {(L__EDGE_TIMER_NBITS){1'b0}};
+reg [L__EDGE_TIMER_NBITS-1:0] s__Rx_edge_cumtol = L__EDGE_TIMER_TOL[0+:L__EDGE_TIMER_NBITS];
+reg s__Rx_edge_timer_zero = 1'b0;
+always @ (posedge i_clk)
+  if (i_rst) begin
+    s__Rx_edge_timer <= {(L__EDGE_TIMER_NBITS){1'b0}};
+    s__Rx_edge_cumtol <= L__EDGE_TIMER_TOL[0+:L__EDGE_TIMER_NBITS];
+    s__Rx_edge_timer_zero <= 1'b0;
+  end else if (s__Rx_input_idle || (s__Rx_waiting && !s__Rx_edge)) begin
+    s__Rx_edge_timer <= {(L__EDGE_TIMER_NBITS){1'b0}};
+    s__Rx_edge_cumtol <= L__EDGE_TIMER_TOL[0+:L__EDGE_TIMER_NBITS];
+    s__Rx_edge_timer_zero <= 1'b0;
+  end else if (s__Rx_edge) begin
+    s__Rx_edge_timer <= L__EDGE_TIMER_START[0+:L__EDGE_TIMER_NBITS];
+    s__Rx_edge_cumtol <= L__EDGE_TIMER_TOL[0+:L__EDGE_TIMER_NBITS];
+    s__Rx_edge_timer_zero <= 1'b0;
+  end else if (s__Rx_edge_timer_zero) begin
+    s__Rx_edge_timer <= L__EDGE_TIMER_START[0+:L__EDGE_TIMER_NBITS];
+    s__Rx_edge_cumtol <= s__Rx_edge_cumtol + L__EDGE_TIMER_TOL[0+:L__EDGE_TIMER_NBITS];
+    s__Rx_edge_timer_zero <= 1'b0;
+  end else begin
+    s__Rx_edge_timer <= s__Rx_edge_timer - { {(L__EDGE_TIMER_NBITS-1){1'b0}}, 1'b1 };
+    s__Rx_edge_cumtol <= s__Rx_edge_cumtol;
+    s__Rx_edge_timer_zero <= (s__Rx_edge_timer == { {(L__EDGE_TIMER_NBITS-1){1'b0}}, 1'b1 });
+  end
+reg s__Rx_edge_error = 1'b0;
+// Edge detector is a composite of incoming edges and fabricated edges.
+assign s__Rx_edge_out = s__Rx_edge || s__Rx_edge_timer_zero;
+// Detect poorly timed edges.  Clear the detection when the input has been idle
+// long enough for there to have been no transmitted data.
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_edge_error <= 1'b0;
+  else if (s__Rx_input_idle)
+    s__Rx_edge_error <= 1'b0;
+  else if (s__Rx_edge && (s__Rx_edge_timer > s__Rx_edge_cumtol))
+    s__Rx_edge_error <= 1'b1;
+  else
+    s__Rx_edge_error <= s__Rx_edge_error;
+// Record the received bit stream after edges occur.
+// Note:  L__NRX is always 4 bits long since NSTOP is either 1 or 2.
+localparam L__NRX = 1+8+@NSTOP@;
+reg [L__NRX-1:0] s__Rx_s = {(L__NRX){1'b1}};
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_s <= {(L__NRX){1'b1}};
+  else if (s__Rx_edge_out)
+    s__Rx_s <= { s__Rx_edge_value, s__Rx_s[1+:L__NRX-1] };
+// State machine:  s__Rx_count == 0 (s__Rx_waiting == 1) means no edges have
+// been encountered.  Otherwise s__Rx_count is the number of edges encountered
+// (at the leading edges of the bits being recorded).  Wait until the first
+// real or fake edge following the last stop bit to record the data (to ensure
+// it isn't glitched in the middle).
+reg s__Rx_error_p;
+initial s__Rx_waiting = 1'b1;
+reg [3:0] s__Rx_count = 4'd0;
+reg s__Rx_wr = 1'b0;
+always @ (posedge i_clk)
+  if (i_rst) begin
+    s__Rx_waiting <= 1'b1;
+    s__Rx_count <= 4'd0;
+    s__Rx_wr <= 1'b0;
+  end else begin
+    s__Rx_waiting <= s__Rx_waiting;
+    s__Rx_wr <= 1'b0;
+    if (s__Rx_edge_out)
+      if (s__Rx_count < L__NRX[0+:4]) begin
+        s__Rx_waiting <= 1'b0;
+        s__Rx_count <= s__Rx_count + 4'd1;
+      end else begin
+        if (s__Rx_deglitched == 1'b0) // immediate start bit
+          s__Rx_count <= 4'd1;
+        else begin
+          s__Rx_waiting <= 1'b1;
+          s__Rx_count <= 4'd0;
+        end
+        s__Rx_wr <= !s__Rx_error_p && !s__Rx_error;
+      end
+    else
+      s__Rx_count <= s__Rx_count;
+  end
+// Check for bad bit sequence.
+initial s__Rx_error_p = 1'b0;
+wire [L__NRX-1:0] s__Rx_p = { s__Rx_edge_value, s__Rx_s[1+:L__NRX-1] };
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_error_p <= 1'b0;
+  else if (s__Rx_input_idle)
+    s__Rx_error_p <= 1'b0;
+  else if ((s__Rx_count == L__NRX[0+:4]) && ((s__Rx_p[0] != 1'b0) || ~&s__Rx_p[L__NRX-1:9]))
+    s__Rx_error_p <= 1'b1;
+  else
+    s__Rx_error_p <= s__Rx_error_p;
+// Optional FIFO
+reg s__Rx_inbuf_error = 1'b0;
+localparam L__INFIFO = @INFIFO@;
+if (L__INFIFO == 0) begin : gen__nofifo
+  initial s__Rx_empty = 1'b1;
+  always @ (posedge i_clk)
+    if (i_rst) begin
+      s__Rx_empty <= 1'b1;
+      s__Rx <= 8'h00;
+    end else begin
+      if (s__Rx_wr)
+        s__Rx <= s__Rx_s[1+:8];
+      else
+        s__Rx <= s__Rx;
+      if (s__Rx_wr) begin
+        if (s__Rx_rd)
+          s__Rx_empty <= s__Rx_empty;
+        else
+          s__Rx_empty <= 1'b0;
+      end else begin
+        if (s__Rx_rd)
+          s__Rx_empty <= 1'b1;
+        else
+          s__Rx_empty <= s__Rx_empty;
+      end
+    end
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_inbuf_error <= 1'b0;
+    else
+      s__Rx_inbuf_error <= (s__Rx_empty && s__Rx_rd) || (!s__Rx_empty && s__Rx_wr&& !s__Rx_rd );
+end else begin : gen__fifo
+  localparam L__INFIFO_NBITS = $clog2(L__INFIFO);
+  reg [L__INFIFO_NBITS:0] s__Rx_fifo_addr_in;
+  reg [L__INFIFO_NBITS:0] s__Rx_fifo_addr_out;
+  wire s__Rx_shift;
+  reg s__Rx_fifo_has_data = 1'b0;
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_fifo_has_data <= 1'b0;
+    else
+      s__Rx_fifo_has_data <= (s__Rx_fifo_addr_out != s__Rx_fifo_addr_in) && !s__Rx_shift;
+  initial s__Rx_empty = 1'b1;
+  always @ (posedge i_clk)
+    if (i_rst) begin
+      s__Rx_empty <= 1'b1;
+    end else begin
+      case ({ s__Rx_fifo_has_data, s__Rx_empty, s__Rx_rd })
+        3'b000 :  s__Rx_empty <= 1'b0;
+        3'b001 :  s__Rx_empty <= 1'b1; // good read
+        3'b010 :  s__Rx_empty <= 1'b1;
+        3'b011 :  s__Rx_empty <= 1'b1; // bad read
+        3'b100 :  s__Rx_empty <= 1'b0;
+        3'b101 :  s__Rx_empty <= 1'b0; // shift, good read
+        3'b110 :  s__Rx_empty <= 1'b0; // shift
+        3'b111 :  s__Rx_empty <= 1'b1; // shift, bad read
+      endcase
+    end
+  assign s__Rx_shift = s__Rx_fifo_has_data && (s__Rx_empty || s__Rx_rd);
+  reg s__Rx_full = 1'b0;
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_full <= 1'b0;
+    else
+      s__Rx_full <= (s__Rx_fifo_addr_in == (s__Rx_fifo_addr_out ^ { 1'b1, {(L__INFIFO_NBITS){1'b0}} }));
+  reg [7:0] s__Rx_fifo_mem[@INFIFO@-1:0];
+  initial s__Rx_fifo_addr_in = {(L__INFIFO_NBITS+1){1'b0}};
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_fifo_addr_in <= {(L__INFIFO_NBITS+1){1'b0}};
+    else if (s__Rx_wr && (!s__Rx_full || s__Rx_shift)) begin
+      s__Rx_fifo_addr_in <= s__Rx_fifo_addr_in + { {(L__INFIFO_NBITS){1'b0}}, 1'b1 };
+      s__Rx_fifo_mem[s__Rx_fifo_addr_in[0+:L__INFIFO_NBITS]] <= s__Rx_s[1+:8];
+    end else
+      s__Rx_fifo_addr_in <= s__Rx_fifo_addr_in;
+  initial s__Rx_fifo_addr_out = {(L__INFIFO_NBITS+1){1'b0}};
+  always @ (posedge i_clk)
+    if (i_rst) begin
+      s__Rx_fifo_addr_out <= {(L__INFIFO_NBITS+1){1'b0}};
+      s__Rx <= 8'h00;
+    end else if (s__Rx_shift) begin
+      s__Rx_fifo_addr_out <= s__Rx_fifo_addr_out + { {(L__INFIFO_NBITS){1'b0}}, 1'b1 };
+      s__Rx <= s__Rx_fifo_mem[s__Rx_fifo_addr_out[0+:L__INFIFO_NBITS]];
+    end else begin
+      s__Rx_fifo_addr_out <= s__Rx_fifo_addr_out;
+    end
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Rx_inbuf_error <= 1'b0;
+    else
+      s__Rx_inbuf_error <= (s__Rx_empty && s__Rx_rd) || (s__Rx_full && s__Rx_wr && !s__Rx_shift);
+end
+// Global error state.
+always @ (posedge i_clk)
+  if (i_rst)
+    s__Rx_error <= 3'h0;
+  else begin
+    // read error
+    if (s__Rx_inbuf_error)
+      s__Rx_error[0] <= 1'b1;
+    else if (s__Rx_error_rd)
+      s__Rx_error[0] <= 1'b0;
+    else
+      s__Rx_error[0] <= s__Rx_error[0];
+    // edge rate error
+    if (s__Rx_edge_error)
+      s__Rx_error[1] <= 1'b1;
+    else if (s__Rx_error_rd)
+      s__Rx_error[1] <= 1'b0;
+    else
+      s__Rx_error[1] <= s__Rx_error[1];
+    // missing start or stop bit(s)
+    if (s__Rx_edge_out && (s__Rx_count == L__NRX[0+:4]) && s__Rx_error_p)
+      s__Rx_error[2] <= 1'b1;
+    else if (s__Rx_error_rd)
+      s__Rx_error[2] <= 1'b0;
+    else
+      s__Rx_error[2] <= s__Rx_error[2];
+  end
+endgenerate
