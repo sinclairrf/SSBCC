880db7f9bbfd4999abc4c25bd41a2858dba5d04b
Rodney Sinclair <rsinclair@sinclairrf.com>
2013-04-09 19:12:35 -0700

    Rework UART_Tx to match UART_Rx coding style and create bidirectional UART from the Rx and Tx cores

--- core/9x8/peripherals/UART.py
+++ core/9x8/peripherals/UART.py
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# Copyright 2012-2013, Sinclair R.F., Inc.
+# Copyright 2013, Sinclair R.F., Inc.
 #
 ################################################################################
 
@@ -10,24 +10,44 @@ import re;
 from ssbccPeripheral import SSBCCperipheral
 from ssbccUtil import SSBCCException;
 
-class UART_Tx(SSBCCperipheral):
+class UART(SSBCCperipheral):
   """
-  Transmit side of a UART:
+  Transmit/receive UART:
     1 start bit
     8 data bits
     1 or 2 stop bits\n
   Usage:
-    PERIPHERAL UART_Tx outport=O_outport_name      \\
+    PERIPHERAL UART    inport=I_inport_name        \\
+                       outport=O_outport_name      \\
+                       inempty=I_inempty_name      \\
+                       inerror=I_inerror_name      \\
                        outstatus=I_outstatus_name  \\
                        baudmethod={clk/rate|count} \\
+                       [insignal=i_name]           \\
                        [outsignal=o_name]          \\
+                       [noSync|sync=n]             \\
+                       [noDeglitch|deglitch=n]     \\
+                       [noInFIFO|inFIFO=n]         \\
                        [noOutFIFO|outFIFO=n]       \\
-                       [nStop={1|2}]\n
+                       [nStop={1|2}]               \\
+                       [edgetol=x]                 \n
   Where:
+    inport=I_inport_name
+      specifies the symbol used by the inport instruction to read a received by
+      from the peripheral
+      Note:  The name must start with "I_".
     outport=O_outport_name
       specifies the symbol used by the outport instruction to write a byte to
       the peripheral
       Note:  The name must start with "O_".
+    inempty=I_inempty_name
+      specifies the symbol used by the inport instruction to get the empty
+      status of the input side of the peripheral
+      Note:  The name must start with "I_".
+    inerror=I_inerror_name
+      specified the symbol used by the inport instruction to get the error
+      status of the input side of the peripheral
+      Note:  The name must start with "I_".
     outstatus=I_outstatus_name
       specifies the symbol used by the inport instruction to get the status of
       the output side of the peripheral
@@ -50,9 +70,32 @@ class UART_Tx(SSBCCperipheral):
              "baudmethod=G_CLK_FREQ_HZ/9600".
       Note:  The numeric values can have Verilog-style '_' separators between
              the digits.  For example, 100_000_000 represents 100 million.
+    insignal=i_name
+      optionally specifies the name of the single-bit transmit signal
+      Default:  i_UART_Rx
     outsignal=o_name
       optionally specifies the name of the output signal
       Default:  o_UART_Tx
+    noSync
+      optionally state no synchronization or registration is performed on the
+      input signal.
+    sync=n
+      optionally state that an n-bit synchronizer will be performed on the
+      input signal.
+      Note:  sync=3 is the default.
+    noDeglitch
+      optionally state that no deglitching is performed on the input signal.
+      Note:  This is the defalt.
+    deglitching=n
+      optionally state that an n-bit deglitcher is performed on the input signal
+      Note:  Deglitching consists of changing the output state when n
+             successive input bits are in the opposite state.
+    noInFIFO
+      optionally state that the peripheral will not have an input FIFO
+      Note:  This is the default.
+    inFIFO=n
+      optionally add a FIFO of depth n to the input side of the UART
+      Note:  n must be a power of 2.
     noOutFIFO
       optionally state that the peripheral will not have an output FIFO
       Note:  This is the default.
@@ -63,13 +106,56 @@ class UART_Tx(SSBCCperipheral):
       optionally configure the peripheral for n stop bits
       default:  1 stop bit
       Note:  n must be 1 or 2
-      Note:  the peripheral does not accept 1.5 stop bits\n
+      Note:  the peripheral does not accept 1.5 stop bits
+    edgetol=x
+      optionally specify the tolerance for edge detection/generation for the
+      deserializer as x%
+      Note:  The default is 2.5%
+      Note:  The tolerance is cumulative as edges are missed, so a tolerance
+             that accumulates to 50% or more in 9 edges, i.e., about 5.5%, is
+             excessive.  A reasonable limit is about half of that, i.e., about
+             2.5%, so that is what is specified.  Smaller tolerances required
+             the recevied signal and the edge generating using the provided
+             micro controller clock to more closely match the nominal baud rate.\n
+      Note:  The tolerance is limited to be between 0.1 and 5.5 and may be
+             expressed as an integer or as a fraction with a single digit after
+             the decimal.
   The following ports are provided by this peripheral:
+    I_inport_name
+      input a recieved byte from the peripheral
+      Note:  If there is no input FIFO, then this is the last received byte.
+             If there is an input FIFO, then this is the next byte in the FIFO.
+      Note:  If there is an input FIFO and the read would cause a FIFO
+             underflow, this will repeat the last received byte.
     O_outport_name
       output the next 8-bit value to transmit or to queue for transmission
       Note:  If there is no output FIFO or if there is an output FIFO and this
              write would cause a FIFO overflow, then this byte will be
              discarded.
+    I_inempty_name
+      input the empty status of the input side of the peripheral
+      bit 0:  input empty
+        this bit will be high when the input side of the peripheral has one or
+        more bytes read to be read
+        Note:  If there is no FIFO this means that a single byte is ready to be
+               read and has not been read.  If there is an input FIFO this
+               means that there are one or more bytes in the FIFO.
+        Note:  "Empty" is used rather than "ready" to facilitate loops that
+               respond when there is a new byte ready to be processed.  See the
+               examples below.
+    I_inerror_name
+      input the error status of the input side of the peripheral
+      Note:  The 3 bits in this value have the following meaning:
+             bit 0 -- a read was performed when no data was available or a new
+                      byte of data was received and the input buffer or FIFO was
+                      already full
+                      Note:  This bit indicates that an error has occured and
+                             one or more bytes is missing in the data stream.
+             bit 1 -- the received edge rate does not match the commanded edge
+                      rate, clock, and tolerance
+                      Note:  This error is cleared by an idle input stream.
+             bit 2 -- missing start bit or stop bit(s) in data stream
+                      Note:  This error is cleared by an idle input stream.
     I_outstatus_name
       input the status of the output side of the peripheral
       bit 0:  output busy
@@ -79,7 +165,7 @@ class UART_Tx(SSBCCperipheral):
                transmitting the last byte.  If there is an output FIFO it means
                that it is full.\n
         Note:  "Busy" is used rather that "ready" to facilitate loops that wait
-               for a not-busy status to send the next byte.  See the examples below.\n
+               for a not-busy status to send the next byte.  See the examples below.
   WARNING:  The peripheral is very simple and does not protect against writing a
             new value in the middle of a transmition or writing to a full FIFO.
             Adding such logic would be contrary to the design principle of
@@ -103,11 +189,22 @@ class UART_Tx(SSBCCperipheral):
       param = param_tuple[0];
       param_arg = param_tuple[1];
       for param_test in (
+          ('deglitch',   r'[1-9]\d*$', int,   ),
+          ('edgetol',    r'(0\.[1-9]|[1-4](\.\d)?|5(\.[0-5])?)$',
+                                       lambda(x):int(10*float(x)+0.5), ),
+          ('inempty',    r'I_\w+$',    None,  ),
+          ('inerror',    r'I_\w+$',    None,  ),
+          ('inport',     r'I_\w+$',    None,  ),
+          ('insignal',   r'i_\w+$',    None,  ),
+          ('noDeglitch', None,         None,  ),
+          ('noInFIFO',   None,         None,  ),
           ('noOutFIFO',  None,         None,  ),
+          ('noSync',     None,         None,  ),
           ('nStop',      r'[12]$',     int,   ),
           ('outport',    r'O_\w+$',    None,  ),
           ('outsignal',  r'o_\w+$',    None,  ),
           ('outstatus',  r'I_\w+$',    None,  ),
+          ('sync',       r'[1-9]\d*$', int,   ),
         ):
         if param == param_test[0]:
           self.AddAttr(config,param,param_arg,param_test[1],ixLine,param_test[2]);
@@ -115,7 +212,7 @@ class UART_Tx(SSBCCperipheral):
       else:
         if param == 'baudmethod':
           self.AddRateMethod(config,param,param_arg,ixLine);
-        elif param in ('outFIFO',):
+        elif param in ('inFIFO','outFIFO',):
           self.AddAttr(config,param,param_arg,r'[1-9]\d*$',ixLine,int);
           x = getattr(self,param);
           if math.modf(math.log(x,2))[0] != 0:
@@ -125,6 +222,9 @@ class UART_Tx(SSBCCperipheral):
     # Ensure the required parameters are provided.
     for paramname in (
         'baudmethod',
+        'inempty',
+        'inerror',
+        'inport',
         'outport',
         'outstatus',
       ):
@@ -132,6 +232,8 @@ class UART_Tx(SSBCCperipheral):
         raise SSBCCException('Required parameter "%s" is missing at line %d' % (paramname,ixLine,));
     # Set optional parameters.
     for optionalpair in (
+        ('edgetol',   25,          ),
+        ('insignal',  'i_UART_Rx', ),
         ('nStop',     1,           ),
         ('outsignal', 'o_UART_Tx', ),
       ):
@@ -139,6 +241,9 @@ class UART_Tx(SSBCCperipheral):
         setattr(self,optionalpair[0],optionalpair[1]);
     # Ensure exclusive pair configurations are set and consistent.
     for exclusivepair in (
+        ('noSync',     'sync',     'sync',       3,    ),
+        ('noDeglitch', 'deglitch', 'noDeglitch', True, ),
+        ('noInFIFO',   'inFIFO',   'noInFIFO',   True, ),
         ('noOutFIFO',  'outFIFO',  'noOutFIFO',  True, ),
       ):
       if hasattr(self,exclusivepair[0]) and hasattr(self,exclusivepair[1]):
@@ -151,10 +256,27 @@ class UART_Tx(SSBCCperipheral):
     # Set the string used to identify signals associated with this peripheral.
     self.namestring = self.outsignal;
     # Add the I/O port, internal signals, and the INPORT and OUTPORT symbols for this peripheral.
+    config.AddIO(self.insignal,1,'input',ixLine);
     config.AddIO(self.outsignal,1,'output',ixLine);
+    config.AddSignal('s__%s__Rx'          % self.namestring,8,ixLine);
+    config.AddSignal('s__%s__Rx_empty'    % self.namestring,1,ixLine);
+    config.AddSignal('s__%s__Rx_error'    % self.namestring,3,ixLine);
+    config.AddSignal('s__%s__Rx_error_rd' % self.namestring,1,ixLine);
+    config.AddSignal('s__%s__Rx_rd'       % self.namestring,1,ixLine);
     config.AddSignal('s__%s__Tx'          % self.namestring,8,ixLine);
     config.AddSignal('s__%s__Tx_busy'     % self.namestring,1,ixLine);
     config.AddSignal('s__%s__Tx_wr'       % self.namestring,1,ixLine);
+    config.AddInport((self.inport,
+                    ('s__%s__Rx'          % self.namestring,8,'data',),
+                    ('s__%s__Rx_rd'       % self.namestring,1,'strobe',),
+                   ),ixLine);
+    config.AddInport((self.inempty,
+                   ('s__%s__Rx_empty'     % self.namestring,1,'data',),
+                  ),ixLine);
+    config.AddInport((self.inerror,
+                   ('s__%s__Rx_error'     % self.namestring,3,'data',),
+                   ('s__%s__Rx_error_rd'  % self.namestring,1,'strobe',),
+                  ),ixLine);
     config.AddOutport((self.outport,
                    ('s__%s__Tx'           % self.namestring,8,'data',),
                    ('s__%s__Tx_wr'        % self.namestring,1,'strobe',),
@@ -166,13 +288,18 @@ class UART_Tx(SSBCCperipheral):
     config.functions['clog2'] = True;
 
   def GenVerilog(self,fp,config):
-    for bodyextension in ('.v',):
+    for bodyextension in ('_Rx.v','_Tx.v',):
       body = self.LoadCore(self.peripheralFile,bodyextension);
       for subpair in (
                     (r'\bL__',          'L__@NAME@__', ),
                     (r'\bgen__',        'gen__@NAME@__', ),
                     (r'\bs__',          's__@NAME@__', ),
+                    (r'@INPORT@',       self.insignal, ),
                     (r'@BAUDMETHOD@',   str(self.baudmethod), ),
+                    (r'@EDGETOL@',      str(self.edgetol), ),
+                    (r'@SYNC@',         str(self.sync), ),
+                    (r'@DEGLITCH@',     str(self.deglitch), ),
+                    (r'@INFIFO@',       str(self.inFIFO), ),
                     (r'@NSTOP@',        str(self.nStop), ),
                     (r'@OUTFIFO@',      str(self.outFIFO), ),
                     (r'@NAME@',         self.namestring, ),
--- core/9x8/peripherals/UART_Tx.v
+++ core/9x8/peripherals/UART_Tx.v
@@ -3,120 +3,123 @@
 //
 generate
 reg  [7:0] s__Tx_data;
-reg        s__go;
-reg        s__uart_busy;
-if (@FIFO@ == 0) begin : gen_@NAME@_nofifo
-  always @ (s__uart_busy)
-    s__busy = s__uart_busy;
+reg        s__Tx_go;
+reg        s__Tx_uart_busy;
+if (@OUTFIFO@ == 0) begin : gen__nooutfifo
+  always @ (s__Tx_uart_busy)
+    s__Tx_busy = s__Tx_uart_busy;
   always @ (s__Tx)
     s__Tx_data = s__Tx;
-  always @ (s__wr)
-    s__go = s__wr;
-end else begin : gen_@NAME@_fifo
-  localparam L__FIFO_NBITS = $clog2(@FIFO@);
-  reg [7:0] s__fifo_mem[@FIFO@-1:0];
-  reg [L__FIFO_NBITS:0] s__fifo_addr_in = {(L__FIFO_NBITS+1){1'b0}};
+  always @ (s__Tx_wr)
+    s__Tx_go = s__Tx_wr;
+end else begin : gen__outfifo
+  localparam L__OUTFIFO_NBITS = $clog2(@OUTFIFO@);
+  reg [7:0] s__Tx_fifo_mem[@OUTFIFO@-1:0];
+  reg [L__OUTFIFO_NBITS:0] s__Tx_fifo_addr_in = {(L__OUTFIFO_NBITS+1){1'b0}};
   always @ (posedge i_clk)
     if (i_rst)
-      s__fifo_addr_in <= {(L__FIFO_NBITS+1){1'b0}};
-    else if (s__wr) begin
-      s__fifo_addr_in <= s__fifo_addr_in + { {(L__FIFO_NBITS){1'b0}}, 1'b1 };
-      s__fifo_mem[s__fifo_addr_in[0+:L__FIFO_NBITS]] <= s__Tx;
-    end
-  reg [L__FIFO_NBITS:0] s__fifo_addr_out;
-  reg s__fifo_has_data = 1'b0;
-  reg s__fifo_full = 1'b0;
+      s__Tx_fifo_addr_in <= {(L__OUTFIFO_NBITS+1){1'b0}};
+    else if (s__Tx_wr) begin
+      s__Tx_fifo_addr_in <= s__Tx_fifo_addr_in + { {(L__OUTFIFO_NBITS){1'b0}}, 1'b1 };
+      s__Tx_fifo_mem[s__Tx_fifo_addr_in[0+:L__OUTFIFO_NBITS]] <= s__Tx;
+    end else
+      s__Tx_fifo_addr_in <= s__Tx_fifo_addr_in;
+  reg [L__OUTFIFO_NBITS:0] s__Tx_fifo_addr_out;
+  reg s__Tx_fifo_has_data = 1'b0;
+  reg s__Tx_fifo_full = 1'b0;
   always @ (posedge i_clk)
     if (i_rst) begin
-      s__fifo_has_data <= 1'b0;
-      s__fifo_full <= 1'b0;
+      s__Tx_fifo_has_data <= 1'b0;
+      s__Tx_fifo_full <= 1'b0;
     end else begin
-      s__fifo_has_data <= (s__fifo_addr_out != s__fifo_addr_in);
-      s__fifo_full <= (s__fifo_addr_out == (s__fifo_addr_in ^ { 1'b1, {(L__FIFO_NBITS){1'b0}} }));
+      s__Tx_fifo_has_data <= (s__Tx_fifo_addr_out != s__Tx_fifo_addr_in);
+      s__Tx_fifo_full <= (s__Tx_fifo_addr_out == (s__Tx_fifo_addr_in ^ { 1'b1, {(L__OUTFIFO_NBITS){1'b0}} }));
     end
-  initial s__fifo_addr_out = {(L__FIFO_NBITS+1){1'b0}};
+  initial s__Tx_go = 1'b0;
   always @ (posedge i_clk)
     if (i_rst)
-      s__fifo_addr_out <= {(L__FIFO_NBITS+1){1'b0}};
-    else if (s__go)
-      s__fifo_addr_out <= s__fifo_addr_out + { {(L__FIFO_NBITS){1'b0}}, 1'b1 };
-  initial s__go = 1'b0;
-  always @ (posedge i_clk)
-    if (i_rst)
-      s__go <= 1'b0;
-    else if (s__fifo_has_data && !s__uart_busy && !s__go)
-      s__go <= 1'b1;
+      s__Tx_go <= 1'b0;
+    else if (s__Tx_fifo_has_data && !s__Tx_uart_busy && !s__Tx_go)
+      s__Tx_go <= 1'b1;
     else
-      s__go <= 1'b0;
+      s__Tx_go <= 1'b0;
+  initial s__Tx_fifo_addr_out = {(L__OUTFIFO_NBITS+1){1'b0}};
+  always @ (posedge i_clk)
+    if (i_rst)
+      s__Tx_fifo_addr_out <= {(L__OUTFIFO_NBITS+1){1'b0}};
+    else if (s__Tx_go)
+      s__Tx_fifo_addr_out <= s__Tx_fifo_addr_out + { {(L__OUTFIFO_NBITS){1'b0}}, 1'b1 };
+    else
+      s__Tx_fifo_addr_out <= s__Tx_fifo_addr_out;
   initial s__Tx_data = 8'd0;
   always @ (posedge i_clk)
     if (i_rst)
       s__Tx_data <= 8'd0;
     else
-      s__Tx_data <= s__fifo_mem[s__fifo_addr_out[0+:L__FIFO_NBITS]];
-  always @ (s__fifo_full)
-    s__busy = s__fifo_full;
+      s__Tx_data <= s__Tx_fifo_mem[s__Tx_fifo_addr_out[0+:L__OUTFIFO_NBITS]];
+  always @ (s__Tx_fifo_full)
+    s__Tx_busy = s__Tx_fifo_full;
 end
 // Count the clock cycles to decimate to the desired baud rate.
 localparam L__COUNT       = @BAUDMETHOD@-1;
 localparam L__COUNT_NBITS = $clog2(L__COUNT+1);
-reg [L__COUNT_NBITS-1:0] s__count = {(L__COUNT_NBITS){1'b0}};
-reg s__count_is_zero = 1'b0;
+reg [L__COUNT_NBITS-1:0] s__Tx_count = {(L__COUNT_NBITS){1'b0}};
+reg s__Tx_count_is_zero = 1'b0;
 always @ (posedge i_clk)
   if (i_rst) begin
-    s__count <= {(L__COUNT_NBITS){1'b0}};
-    s__count_is_zero <= 1'b0;
-  end else if (s__go || s__count_is_zero) begin
-    s__count <= L__COUNT[0+:L__COUNT_NBITS];
-    s__count_is_zero <= 1'b0;
+    s__Tx_count <= {(L__COUNT_NBITS){1'b0}};
+    s__Tx_count_is_zero <= 1'b0;
+  end else if (s__Tx_go || s__Tx_count_is_zero) begin
+    s__Tx_count <= L__COUNT[0+:L__COUNT_NBITS];
+    s__Tx_count_is_zero <= 1'b0;
   end else begin
-    s__count <= s__count - { {(L__COUNT_NBITS-1){1'b0}}, 1'b1 };
-    s__count_is_zero <= (s__count == { {(L__COUNT_NBITS-1){1'b0}}, 1'b1 });
+    s__Tx_count <= s__Tx_count - { {(L__COUNT_NBITS-1){1'b0}}, 1'b1 };
+    s__Tx_count_is_zero <= (s__Tx_count == { {(L__COUNT_NBITS-1){1'b0}}, 1'b1 });
   end
 // Latch the bits to output.
-reg [7:0] s__out_stream = 8'hFF;
+reg [7:0] s__Tx_stream = 8'hFF;
 always @ (posedge i_clk)
   if (i_rst)
-    s__out_stream <= 8'hFF;
-  else if (s__go)
-    s__out_stream <= s__Tx_data;
-  else if (s__count_is_zero)
-    s__out_stream <= { 1'b1, s__out_stream[1+:7] };
+    s__Tx_stream <= 8'hFF;
+  else if (s__Tx_go)
+    s__Tx_stream <= s__Tx_data;
+  else if (s__Tx_count_is_zero)
+    s__Tx_stream <= { 1'b1, s__Tx_stream[1+:7] };
   else
-    s__out_stream <= s__out_stream;
+    s__Tx_stream <= s__Tx_stream;
 // Generate the output bit stream.
 initial @NAME@ = 1'b1;
 always @ (posedge i_clk)
   if (i_rst)
     @NAME@ <= 1'b1;
-  else if (s__go)
+  else if (s__Tx_go)
     @NAME@ <= 1'b0;
-  else if (s__count_is_zero)
-    @NAME@ <= s__out_stream[0];
+  else if (s__Tx_count_is_zero)
+    @NAME@ <= s__Tx_stream[0];
   else
     @NAME@ <= @NAME@;
 // Count down the number of bits.
 localparam L__NTX       = 1+8+@NSTOP@-1;
 localparam L__NTX_NBITS = $clog2(L__NTX);
-reg [L__NTX_NBITS-1:0] s__ntx = {(L__NTX_NBITS){1'b0}};
+reg [L__NTX_NBITS-1:0] s__Tx_n = {(L__NTX_NBITS){1'b0}};
 always @ (posedge i_clk)
   if (i_rst)
-    s__ntx <= {(L__NTX_NBITS){1'b0}};
-  else if (s__go)
-    s__ntx <= L__NTX[0+:L__NTX_NBITS];
-  else if (s__count_is_zero)
-    s__ntx <= s__ntx - { {(L__NTX_NBITS-1){1'b0}}, 1'b1 };
+    s__Tx_n <= {(L__NTX_NBITS){1'b0}};
+  else if (s__Tx_go)
+    s__Tx_n <= L__NTX[0+:L__NTX_NBITS];
+  else if (s__Tx_count_is_zero)
+    s__Tx_n <= s__Tx_n - { {(L__NTX_NBITS-1){1'b0}}, 1'b1 };
   else
-    s__ntx <= s__ntx;
+    s__Tx_n <= s__Tx_n;
 // The status bit is 1 if the core is busy and 0 otherwise.
-initial s__uart_busy = 1'b1;
+initial s__Tx_uart_busy = 1'b1;
 always @ (posedge i_clk)
   if (i_rst)
-    s__uart_busy <= 1'b0;
-  else if (s__go)
-    s__uart_busy <= 1'b1;
-  else if (s__count_is_zero && (s__ntx == {(L__NTX_NBITS){1'b0}}))
-    s__uart_busy <= 1'b0;
+    s__Tx_uart_busy <= 1'b0;
+  else if (s__Tx_go)
+    s__Tx_uart_busy <= 1'b1;
+  else if (s__Tx_count_is_zero && (s__Tx_n == {(L__NTX_NBITS){1'b0}}))
+    s__Tx_uart_busy <= 1'b0;
   else
-    s__uart_busy <= s__uart_busy;
+    s__Tx_uart_busy <= s__Tx_uart_busy;
 endgenerate
