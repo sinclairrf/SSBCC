3dd36a912b0f019786c1f7ec774cb6d781d98f79
Rodney Sinclair <rsinclair@sinclairrf.com>
2013-10-10 07:01:08 -0700

    Add method to extract bit slices as per Verilog descriptions and add unit test

--- ssbccUtil.py
+++ ssbccUtil.py
@@ -7,6 +7,7 @@
 ################################################################################
 
 import math
+import re
 
 ################################################################################
 #
@@ -45,8 +46,52 @@ def CeilPow2(v):
   """
   return 2**CeilLog2(v);
 
+def ExtractBits(v,bits):
+  """
+  Extract the bits specified by bits from v.
+  bits must have a Verilog-type format.  I.e., [7:0], [0+:8], etc.
+  """
+  if type(v) != int:
+    raise SSBCCException('%s must be an int' % v);
+  if re.match(r'[[]\d+:\d+]$',bits):
+    cmd = re.findall(r'[[](\d+):(\d+)]$',bits)[0];
+    b0 = int(cmd[1]);
+    bL = int(cmd[0]) - b0 + 1;
+  elif re.match(r'[[]\d+\+:\d+]$',bits):
+    cmd = re.findall(r'[[](\d+)\+:(\d+)]$',bits)[0];
+    b0 = int(cmd[0]);
+    bL = int(cmd[1]);
+  else:
+    raise SSBCCException('Unrecognized bit slice format:  %s' % bits);
+  if not 1 <= bL <= 8:
+    raise SSBCCException('Malformed range "%s" doesn\'t provide 1 to 8 bits' % bits)
+  v /= 2**b0;
+  v %= 2**bL;
+  return v;
+
 def IsPowerOf2(v):
   """
   Indicate whether or not the argument is a power of 2.
   """
   return v == 2**int(math.log(v,2)+0.5);
+
+################################################################################
+#
+# Unit test.
+#
+################################################################################
+
+if __name__ == "__main__":
+
+  def Test_ExtractBits(v,bits,vExpect):
+    vGot = ExtractBits(v,bits);
+    if vGot != vExpect:
+      raise Exception('ExtractBits failed: 0x%04X %s ==> 0x%02X instead of 0x%02X' % (v,bits,ExtractBits(v,bits),vExpect,));
+
+  for v in (256,257,510,):
+    Test_ExtractBits(v,'[0+:8]',v%256);
+    Test_ExtractBits(v,'[7:0]',v%256);
+    Test_ExtractBits(v,'[4+:6]',(v/16)%64);
+    Test_ExtractBits(v,'[9:4]',(v/16)%64);
+
+  print 'Unit test passed';
