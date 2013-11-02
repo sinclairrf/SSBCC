864ed401c57bf6efec1e4246991c6f051ac125d4
Rodney Sinclair <rsinclair@sinclairrf.com>
2013-11-02 07:23:48 -0700

    Make the diagnostic terminate if the terminating write isn't detected

--- core/9x8/peripherals/tb/AXI4_Lite_Slave_DualPortRAM/tb_AXI4_Lite_Slave_DualPortRAM.s
+++ core/9x8/peripherals/tb/AXI4_Lite_Slave_DualPortRAM/tb_AXI4_Lite_Slave_DualPortRAM.s
@@ -3,19 +3,15 @@
 
 .main
 
-  ; Clear the dual-port-ram from the bottom to the top
+  ; Set the dual-port-ram to the non-zero value 0xAB from the bottom to the top.
   0 :clear O_DP_ADDRESS outport 0xAB .outport(O_DP_WRITE) 1+ dup L_DP_SIZE - .jumpc(clear) drop
 
-;  :forever 0 :read O_DP_ADDRESS outport .inport(I_DP_READ) .outport(O_DIAG_DATA) 1+ dup L_DP_SIZE - .jumpc(read) drop .jump(forever)
-
-
-  ; Wait for the AXI master to write a 4 to address 16.
-  16 .outport(O_DP_ADDRESS)
-  :wait_4 .inport(I_DP_READ) 4 - .jumpc(wait_4)
-
-
-;  ; Clear the 32-bit word starting at address 4.
-;  4 ${4-1} :clear_word >r O_DP_ADDRESS outport 0 .outport(O_DP_WRITE) 1+ r> .jumpc(clear_word,1-) drop drop
+  ; Wait up to 128 iterations for the AXI master to write a 4 to address 16
+  0x10 .outport(O_DP_ADDRESS)
+  ${128-1} :wait_4
+    .inport(I_DP_READ) 4 - 0= .jumpc(wait_4_done) .jumpc(wait_4,1-) drop .outstrobe(O_DONE) :wait_4_inf .jump(wait_4_inf)
+  :wait_4_done
+    drop
 
   ; Read and output the first 20 memory addresses starting with address 0.
   0 ${20-1} :read_16 >r
@@ -23,6 +19,6 @@
     1+ r> .jumpc(read_16,1-) drop
 
   ; Terminate the program.
-  O_DONE outport
+  .outstrobe(O_DONE)
 
   :infinite .jump(infinite)
