256d6991de23eb9013f2e37786d2c39d073c2baf
Rodney Sinclair <rodney@sinclairrf.com>
2015-08-18 08:43:20 -0700

    Fix description and fix byte selection computation

--- macros/9x8/pushByte.py
+++ macros/9x8/pushByte.py
@@ -2,14 +2,14 @@
 
 def pushByte(ad):
   """
-  User-defined macro to push a 32 bit value onto the data stack so that the LSB
-  is deepest in the data stack and the MSB is at the top of the data stack.
+  User-defined macro to push a specific byte of a multi-byte onto the data
+  stack.\n
   Usage:
     .pushByte(v,ix)
   where
     v           is a multi-byte value, a constant, or an evaluated expression
     ix          is the index to the byte to push (ix=0 ==> push the LSB, ...)\n
-  The effect is to push "(v/2**ix) % 0x10" onto the data stack.\n
+  The effect is to push "(v/256**ix) % 0x100" onto the data stack.\n
   ( - u_byte )
   """
 
@@ -25,7 +25,7 @@ def pushByte(ad):
     ix = ad.Emit_IntegerValue(argument[1]);
     if ix < 0:
       raise asmDef.AsmException('ix must be non-negative in .pushByte at %s' % argument[1]['loc']);
-    v = int(v/2**ix) % 0x100;
+    v = int(v/256**ix) % 0x100;
     printValue = argument[0]['value'] if type(argument[0]['value']) == str else '0x%X' % argument[0]['value'];
     printIx    = argument[1]['value'] if type(argument[1]['value']) == str else '0x%X' % argument[1]['value'];
     ad.EmitPush(fp,v,'.pushByte(%s,%s)' % (printValue,printIx,));
